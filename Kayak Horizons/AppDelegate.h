1dc0d37c7d6d64071054b438daec43b40cbe13df
Joe Krueger <joekrueger@andrew-pc.gateway.2wire.net>
2013-02-17 23:29:59 -0800

    Initial Commit

---
+++ Kayak Horizons/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Kayak Horizons
+//
+//  Created by Joe Krueger on 2/17/13.
+//  Copyright (c) 2013 Kayak Horizons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Kayak Horizons/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Kayak Horizons
+//
+//  Created by Joe Krueger on 2/17/13.
+//  Copyright (c) 2013 Kayak Horizons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
